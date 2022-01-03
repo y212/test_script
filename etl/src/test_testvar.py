5bd8287c88d1c0c1600fdb8aaa814088dff4648a
Yesh maheswari <Yeshm@HEXAVIEWNET.LOCAL>
2022-01-03 21:54:01 +0530

    testing sub-files

--- etl/src/test_testvar.py
+++ etl/src/test_testvar.py
@@ -1,10 +1,10 @@
 import unittest
-import etl.src as testvar
+from etl.src.testvar import main
 
 
 class Testing(unittest.TestCase):
     def test_test_var(self):
-        x = testvar.main(['--name', 'bhanu_new', '--age', '21'])
+        x = main(['--name', 'bhanu_new', '--age', '21'])
         self.assertEqual(x, 'hellobhanu_new21')
 
 
